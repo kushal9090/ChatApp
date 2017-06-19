17b3b9c53512614a7ecba86236d5a332f2576d80
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-19 22:36:51 +0530

    added a line between every chat object and implemented parcelable

---
+++ app/src/main/java/com/kushal/chatapp/ui/chatscreen/ChatScreenActivity.java
@@ -0,0 +1,15 @@
+package com.kushal.chatapp.ui.chatscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kushal.chatapp.R;
+
+public class ChatScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+}
