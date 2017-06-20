477877f83df49345b9ee87891adc17f720e8d24f
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-20 17:30:09 +0530

    added a activity chat

--- app/src/main/java/com/kushal/chatapp/ui/chatscreen/ChatScreenActivity.java
+++ app/src/main/java/com/kushal/chatapp/ui/chatscreen/ChatScreenActivity.java
@@ -2,9 +2,16 @@ package com.kushal.chatapp.ui.chatscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.kushal.chatapp.R;
 import com.kushal.chatapp.ui.chatscreen.models.Chat;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatScreenActivity extends AppCompatActivity {
 
@@ -23,7 +30,7 @@ public class ChatScreenActivity extends AppCompatActivity {
         initView();
     }
     private void  initView(){
-
+      initToolbar();
     }
 
     private Chat mChat;
@@ -35,6 +42,28 @@ public class ChatScreenActivity extends AppCompatActivity {
 
       setTitle(mChat.getUser().getName());
         }
+
+    }
+    private void initToolbar(){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView nameTV = (TextView) findViewById(R.id.nameTV);
+        nameTV.setText(mChat.getUser().getName());
+
+        CircleImageView userIV = (CircleImageView) findViewById(R.id.userIV);
+        Picasso.with(this).load(mChat.getUser().getProfilePicUrl()).into(userIV);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Toolbar","Clicked");
+            }
+        });
+
+
 
     }
 }
