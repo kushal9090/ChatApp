85fb84f92962b4c4c293c4be56e722d7ed49d42e
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-25 16:15:30 +0530

    added icons in chat activity

--- app/src/main/java/com/kushal/chatapp/ui/chatscreen/ChatScreenActivity.java
+++ app/src/main/java/com/kushal/chatapp/ui/chatscreen/ChatScreenActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
@@ -65,5 +66,17 @@ public class ChatScreenActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_chat , menu);
+        return true;
     }
 }
