236fc4ea0a493f12d252eff18173f1d1a63e32d3
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-29 13:24:35 +0530

    toolbar added in userlist section

--- app/src/main/java/com/kushal/chatapp/ui/chatscreen/userlist/UserListActivity.java
+++ app/src/main/java/com/kushal/chatapp/ui/chatscreen/userlist/UserListActivity.java
@@ -1,27 +1,39 @@
 package com.kushal.chatapp.ui.chatscreen.userlist;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
 import com.kushal.chatapp.R;
-import com.kushal.chatapp.ui.chatscreen.ChatScreenFragment;
 
-public class UserListActivity extends AppCompatActivity {
+/**
+ * Created by kusha on 6/29/2017.
+ */
+
+public class UserListActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
-        initView();
+        initViews();
     }
 
-    private void initView() {
+    private void initViews()
+    {
+        initToolbar();
         showFragment();
     }
 
-    private void showFragment() {
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    private void showFragment()
+    {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frameLayout, FragmentUserList.newInstance());
         ft.commit();
